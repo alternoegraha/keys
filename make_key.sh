7b1734a8c150a5e39f7fe20a684642997f1f2219
alternoegraha <noegrahachan@gmail.com>
2026-06-11 01:03:43 +0000

    init

---
+++ make_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+set -u
+bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
+    $1 \
+    '/C=ID/ST=Indonesia/L=Indonesia/O=fckmycmptr/OU=fckmycmptr/CN=fckmycmptr/emailAddress=alternoegraha@proton.me'
